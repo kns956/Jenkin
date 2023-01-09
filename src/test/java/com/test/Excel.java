aa0fa97e84dd3248caf1e6a429eec7a91e650abe
kns956 <knsathyabe956@gmai.com>
2023-01-09 20:55:11 +0530

    first commit

---
+++ src/test/java/com/test/Excel.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Excel {
+	
+	public static void main(String[] args) {
+		
+		Person myObj = new Person();
+		myObj.setName("name");
+		String name = myObj.getName();
+        System.out.println(name);
+        
+	} 
+}
---
+++ src/test/java/com/test/Person.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Person {
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newr) {
+		this.name = newr;
+	}
+	
+}
